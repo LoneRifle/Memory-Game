e3d8583dc04d4801b03cd5336beb06b256a34df6
LoneRifle <LoneRifle@users.noreply.github.com>
2019-03-26 13:49:58 +0800

    Add babel to the build toolchain

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ module.exports = [
   // Javascript
   {
     entry: path.resolve(__dirname, './app/js/index.js'),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          query: {
+            compact: false,
+          },
+        },
+      ],
+    },
     output: {
       path: path.resolve(__dirname, './app/dist'),
       filename: 'bundle.js',
